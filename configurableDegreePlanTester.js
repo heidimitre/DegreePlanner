caeed6863c66f8a9874c4b31344cfa49bc3a8606
heidimitre <heidi.mitre@gmail.com>
2016-07-30 12:23:45 -0700

    Added resources and configureableDegreePlanTester to use JSON configurable tests

---
+++ configurableDegreePlanTester.js
@@ -0,0 +1,20 @@
+var courses = require('./resources/degreePlan.json');
+var DegreePlan = require('./degreePlan');
+var Course = require('./course');
+var writePlan = require('./writePlan');
+
+var plan = new DegreePlan(courses.name);
+
+function createPlan(list){
+  list.forEach(function(course){
+    addNewCourse(course);
+  });
+};
+
+function addNewCourse(course){
+  var newCourse = new Course(course.number, course.title, course.creditHours);
+  plan.addCourse(newCourse);
+};
+
+createPlan(courses.list);
+writePlan(plan.courseList);
