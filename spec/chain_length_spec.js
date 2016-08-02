198cca1aa9a50af08231413864d1f8be7f4ce986
heidimitre <heidi.mitre@gmail.com>
2016-08-02 14:00:41 -0700

    Tested depth calculations without giving course number for head

--- spec/chain_length_spec.js
+++ spec/chain_length_spec.js
@@ -1,6 +1,6 @@
 var DegreePlan = require('../degreePlan');
 
-describe('hasDepth', function(){
+describe('calculateDepths', function(){
   it('has the correct depth after the counting method is run.', function(){
     var plan = new DegreePlan('Computer Science');
     plan.addCourse('310', 'x', 3);
@@ -80,3 +80,40 @@ function containsHead(head, headArray){
     return boolean;
   }, false);
 };
+
+describe('calculateDepths', function(){
+  it('has the correct depth if number not given as head course.', function(){
+    var plan = new DegreePlan('CS');
+    plan.addCourse('485', 'x', 3);
+    plan.addCourse('355', 'x', 3);
+    plan.addCourse('360', 'x', 3);
+    plan.addCourse('310', 'x', 3);
+    plan.addCourse('240', 'x', 3);
+    plan.addCourse('105', 'x', 3);
+    plan.addCourse('420', 'x', 3);
+    plan.addCourse('301', 'x', 3);
+    plan.addCourse('400', 'x', 3);
+
+    plan.addPrerequisite('485', '355');
+    plan.addPrerequisite('485', '360');
+    plan.addPrerequisite('355', '310');
+    plan.addPrerequisite('360', '240');
+    plan.addPrerequisite('310', '240');
+    plan.addPrerequisite('420', '301');
+    plan.addPrerequisite('420', '400');
+
+    var heads = plan.findHeads();
+    for(var i = 0; i < heads.length; i++)
+    {
+      plan.calculateDepths(heads[i].number);
+    }
+
+    expect(plan.getMaxDepth("485")).toBe(0);
+    expect(plan.getMaxDepth("355")).toBe(1);
+    expect(plan.getMaxDepth("310")).toBe(2);
+    expect(plan.getMaxDepth("240")).toBe(3);
+    expect(plan.getMaxDepth("420")).toBe(0);
+    expect(plan.getMaxDepth("301")).toBe(1);
+    expect(plan.getMaxDepth("105")).toBe(0);
+  });
+});
